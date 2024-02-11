02098ebb96470a78e767d56ca9f783e095ef1edd
Antonio <147817129+AntonioTri@users.noreply.github.com>
2024-02-11 01:39:33 +0100

    Update Game.java

--- ProgettoProg3/Game.java
+++ ProgettoProg3/Game.java
@@ -22,7 +22,7 @@ public class Game implements Runnable{
     
 
     //Variabili per la mappa
-    public final static float SCALE = 2.5f;
+    public final static float SCALE = 1.7f;
     public final static int TILES_DEFAULT_SIZE = 32;
     public final static int TILES_IN_WIDTH = 26;
     public final static int TILES_IN_HEIGHT = 14;
